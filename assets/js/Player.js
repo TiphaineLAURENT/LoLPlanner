fe786d729df4a584aa5fe0f0f78257c2c49ac879
Tiphaine LAURENT <tlaurent@ixemelis.fr>
2019-09-11 18:05:59 +0200

    Add chmapion setter and getter

--- assets/js/Player.js
+++ assets/js/Player.js
@@ -1,10 +1,18 @@
 class Player {
   constructor() {
     this.node = document.querySelector("#player");
-    this.profileIcon = this.node.querySelector("#profileIcon");
-    this.champion = this.node.querySelector("#champion");
-    this.summonerName = this.node.querySelector("#summonerName");
-    this.summonerSpells = this.node.querySelectorAll(".summonerSpell");
-    this.score = this.node.querySelector("#score");
+    this._profileIcon = this.node.querySelector("#profileIcon");
+    this._champion = this.node.querySelector("#champion");
+    this._summonerName = this.node.querySelector("#summonerName");
+    this._summonerSpells = this.node.querySelectorAll(".summonerSpell");
+    this._score = this.node.querySelector("#score");
+  }
+
+  set champion(name) {
+    this._champion.textContent = name;
+  }
+
+  get champion() {
+    return this._champion.textContent;
   }
 }
